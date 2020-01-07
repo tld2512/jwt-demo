67030d4af7e77cc5b39eebc9a0c297e2f9224c12
tld2512 <tienlongdinh251297@gmail.com>
2020-01-07 16:10:33 +0700

    first commit

---
+++ src/main/java/com/demojwt/DemojwtApplication.java
@@ -0,0 +1,15 @@
+package com.demojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class DemojwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojwtApplication.class, args);
+    }
+
+}
